05cf1d789ec2373b86fc8121a6346bbc87a9412f
Matheus Henrique de Oliveira <82242444+matheushro@users.noreply.github.com>
2021-06-25 20:13:04 -0300

    Add files via upload

--- 01.cpp
+++ 01.cpp
@@ -1,55 +1,26 @@
 #include<iostream>
 
+float soma (float n1, float n2){
+	return n1+n2;
+}
+float sub (float n1, float n2){
+	return n1-n2;
+}
+float div (float n1, float n2){
+	return n1/n2;
+}
+float mult (float n1, float n2){
+	return n1*n2;
+}
 int main(){
-	int e;
-	float r;
-	inicio:
-	std::cout << "Digite 1 para adicao\nDigite 2 para subtracao\nDigite 3 para multiplicacao\nDigite 4 para divisao\n";
-	std::cin >> e;
-	if(e==1){
-		std::cout << "Digite o primeiro valor:";
-		float v;
-		std::cin >> v;
-		std::cout << "Digite o segundo valor:";
-		float a;
-		std::cin >> a;
-		r = v+a;
-		std::cout << "O resultado da adicao e: " << r << "!\n";
-	}
-	else if(e==2){
-		std::cout << "Digite o primeiro valor:";
-		float v;
-		std::cin >> v;
-		std::cout << "Digite o segundo valor:";
-		float a;
-		std::cin >> a;
-		r = a-v;
-		std::cout << "O resultado da subtracao e: " << r << "!\n";
-	}
-	else if(e==3){
-		std::cout << "Digite o primeiro valor:";
-		float v;
-		std::cin >> v;
-		std::cout << "Digite o segundo valor:";
-		float a;
-		std::cin >> a;
-		r = v*v;
-		std::cout << "O resultado da multiplicacao e: " << r << "!\n";
-	}
-	else if(e==4){
-		std::cout << "Digite o primeiro valor:";
-		float v;
-		std::cin >> v;
-		std::cout << "Digite o segundo valor:";
-		float a;
-		std::cin >> a;
-		r = a/v;
-		std::cout << "O resultado da divisao e: " << r << "!\n";                                          
-	}
-		else{
-		system("cls");
-		std::cout << "Essa opcao nao existe!\nDigite um numero entre 1 e 4!\n";
-		goto inicio;
-	}
-	return 0;
+	float n2;
+	float n1;
+	std::cout << "Digite o primeiro valor:";
+	std::cin >> n1;
+	std::cout << "Digite o segundo valor:";
+	std::cin >> n2;
+	std::cout << "Soma : " << soma(n1,n2) << "\n";
+	std::cout << "Subtracao : " << sub(n1,n2) << "\n";
+	std::cout << "Multiplicacao : " << mult(n1,n2) << "\n";
+	std::cout << "Divisao : " << div(n1,n2) << "\n";
 }
--- 04.cpp
+++ 04.cpp
@@ -1,45 +1,52 @@
 #include<iostream>
-
+float quad(float v){
+	return v*v;
+}
+float ret (float v, float a){
+	return v*a;
+}
+float circ (float v, float pi){
+	 return v*v*pi;
+}
+float tri (float a, float v){
+	return a*v/2;
+}
 int main(){
 	int e;
-	int r;
 	inicio:
 	std::cout << "Digite 1 para calcular a area do quadrado\nDigite 2 para calcular a area do Retangulo\nDigite 3 para calcular a area do Circulo\nDigite 4 para calcular a area do Triangulo\n";
 	std::cin >> e;
 	if(e==1){
 		std::cout << "Digite o valor dos lados do quadrado:";
-		int v;
+		float v;
 		std::cin >> v;
-		r = v*v;
-		std::cout << "A area do quadrado e: " << r << "!\n";
+		std::cout << "A area do quadrado e: " << quad(v) << "!\n";
 	}
 	else if(e==2){
 		std::cout << "Digite o valor da base do retangulo:";
-		int v;
+		float v;
 		std::cin >> v;
 		std::cout << "Digite o valor da altura do retangulo:";
-		int a;
+		float a;
 		std::cin >> a;
-		r = a*v;
-		std::cout << "A area do retangulo e: " << r << "!\n";
+		std::cout << "A area do retangulo e: " << ret(v,a) << "!\n";
 	}
 	else if(e==3){
 		std::cout << "O valor de PI e 3,14\n";
 		std::cout << "Digite o valor do raio do circulo:";
 		int v;
 		std::cin >> v;
-		r = v*v*3,14;
-		std::cout << "A area do circulo e: " << r << "!\n";
+		float pi = 3.14;
+		std::cout << "A area do circulo e: " << circ(v,pi) << "!\n";
 	}
 	else if(e==4){
 		std::cout << "Digite o valor da base do triangulo:";
-		int v;
+		float v;
 		std::cin >> v;
 		std::cout << "Digite o valor da altura do triangulo:";
-		int a;
+		float a;
 		std::cin >> a;
-		r = a*v/2;
-		std::cout << "A area do triangulo e: " << r << "!\n";
+		std::cout << "A area do triangulo e: " << tri(a,v) << "!\n";
 	}
 	else{
 		system("cls");
